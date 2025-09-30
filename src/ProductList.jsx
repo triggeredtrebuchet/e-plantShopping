0d15ba574e9ee056674adafff496f0000756a2ce
Hubert Baraniak <hubert.baraniak@unitedrobots.co>
2025-09-30 17:16:04 +0200

    ready

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
+import { useSelector, useDispatch } from 'react-redux';
 function ProductList({ onHomeClick }) {
+    const dispatch = useDispatch();
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
     const handleAddToCart = (product) => {
   dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
-
+console.log("Product added to cart:", product);
   setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
     ...prevState, // Spread the previous state to retain existing entries
     [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
@@ -242,6 +244,9 @@ function ProductList({ onHomeClick }) {
         fontSize: '30px',
         textDecoration: 'none',
     }
+    const calculateTotalQuantity = () => {
+ return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0;
+  };
 
     const handleHomeClick = (e) => {
         e.preventDefault();
@@ -305,8 +310,9 @@ function ProductList({ onHomeClick }) {
           <button
             className="product-button"
             onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
+            disabled={addedToCart[plant.name]}
           >
-            Add to Cart
+            {addedToCart[plant.name] ? "Added to Cart" : "Add to Cart"}
           </button>
         </div>
       ))}
